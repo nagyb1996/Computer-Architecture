dc1ded10282e834465f25e9fed78500d0f555ac0
nagyb1996 <nagyb@wit.edu>
2019-07-23 22:20:47 -0400

    Third Input now works
    need to do input 2

--- Assignment5_6/Assignment5_6/Assignment5_6.cpp
+++ Assignment5_6/Assignment5_6/Assignment5_6.cpp
@@ -106,11 +106,13 @@ vector<string> decodeInstruction (string instruction) {
 }
 
 
-void ALU(vector<string> instructionToExecute) {
+int ALU(vector<string> instructionToExecute) {
 	string typeOfInstruction = instructionToExecute[0];
 	string specificInstruction = instructionToExecute[1];
 	int result = 0;
 	int destination = 0;
+	int pcAddress = 0;
+	bool write = true;
 
 	// R type instructions
 	if (typeOfInstruction.compare("R") == 0) {
@@ -134,19 +136,27 @@ void ALU(vector<string> instructionToExecute) {
 	}
 	// D type instructions
 	else if (typeOfInstruction.compare("D") == 0) {
-
+		int immediate = stoi(instructionToExecute[2]);
 	}
 	// C type instructions
 	else if (typeOfInstruction.compare("C") == 0) {
-
+		if (specificInstruction.compare(instructionType[7]) == 0) { // if CBZ
+			int value = stoi(instructionToExecute[4]);
+			if(value == 0)
+				pcAddress = stoi(instructionToExecute[3]);
+		}
+		write = false;
 	}
 	// B type instructions
 	else if (typeOfInstruction.compare("B") == 0) {
-
+		write = false;
+		pcAddress = stoi(instructionToExecute[2]);
 	}
 
-	writeReg(destination, result);
+	if(write)
+		writeReg(destination, result);
 
+	return pcAddress;
 }
 
 
@@ -154,78 +164,95 @@ void ALU(vector<string> instructionToExecute) {
 
 int main()
 {
-	reg[0] = 0; // X0 Arguments/Results
-	reg[1] = 0; // X1 Arguments/Results
-	reg[2] = 0; // X2 Arguments/Results
-	reg[3] = 0; // X3 Arguments/Results
-	reg[4] = 0; // X4 Arguments/Results
-	reg[5] = 0; // X5 Arguments/Results
-	reg[6] = 0; // X6 Arguments/Results
-	reg[7] = 0; // X7 Arguments/Results
-	reg[8] = 0; // X8 indirect result location register
-	reg[9] = 0; // X9 Temp
-	reg[10] = 0; // X10 Temp
-	reg[11] = 0; // X11 Temp
-	reg[12] = 0; // X12 Temp
-	reg[13] = 0; // X13 Temp
-	reg[14] = 0; // X14 Temp
-	reg[15] = 0; // X15 Temp
-	reg[16] = 0; // X16 Temp
-	reg[17] = 0; // X17 Temp
-	reg[18] = 0; // X18 Temp
-	reg[19] = 0; // X19 Saved
-	reg[20] = 0; // X20 Saved
-	reg[21] = 0; // X21 Saved
-	reg[22] = 0; // X22 Saved
-	reg[23] = 0; // X23 Saved
-	reg[24] = 0; // X24 Saved
-	reg[25] = 0; // X25 Saved
-	reg[26] = 0; // X26 Saved
-	reg[27] = 0; // X27 Saved
-	reg[28] = 0; // X28 (SP), Stack Pointer
-	reg[29] = 0; // X29 (FP), Frame Pointer
-	reg[30] = 0; // X30 (LR), Link Register (return address)
-	reg[31] = 0; // XZR Const 0
 
-	// create PC and NPC 
-	int PC = 0;
-	int NPC = 0;
-	// initialize instruction memory
-	vector<string> instructionMem;
-	//create and intialize data memory
-	int dataMem[8] = { 0,0,0,0,0,0,0,0 };
+		reg[0] = 0; // X0 Arguments/Results
+		reg[1] = 0; // X1 Arguments/Results
+		reg[2] = 0; // X2 Arguments/Results
+		reg[3] = 0; // X3 Arguments/Results
+		reg[4] = 0; // X4 Arguments/Results
+		reg[5] = 0; // X5 Arguments/Results
+		reg[6] = 0; // X6 Arguments/Results
+		reg[7] = 0; // X7 Arguments/Results
+		reg[8] = 0; // X8 indirect result location register
+		reg[9] = 0; // X9 Temp
+		reg[10] = 0; // X10 Temp
+		reg[11] = 0; // X11 Temp
+		reg[12] = 0; // X12 Temp
+		reg[13] = 0; // X13 Temp
+		reg[14] = 0; // X14 Temp
+		reg[15] = 0; // X15 Temp
+		reg[16] = 0; // X16 Temp
+		reg[17] = 0; // X17 Temp
+		reg[18] = 0; // X18 Temp
+		reg[19] = 0; // X19 Saved
+		reg[20] = 0; // X20 Saved
+		reg[21] = 0; // X21 Saved
+		reg[22] = 0; // X22 Saved
+		reg[23] = 0; // X23 Saved
+		reg[24] = 0; // X24 Saved
+		reg[25] = 0; // X25 Saved
+		reg[26] = 0; // X26 Saved
+		reg[27] = 0; // X27 Saved
+		reg[28] = 0; // X28 (SP), Stack Pointer
+		reg[29] = 0; // X29 (FP), Frame Pointer
+		reg[30] = 0; // X30 (LR), Link Register (return address)
+		reg[31] = 0; // XZR Const 0
 
-	// read instructions from file into instruction memory
-	ifstream input("input1.txt");
-	string data;
-	if (input.good())
-	{
-		while (getline(input, data)) //while there are lines to read, read them
+		// create PC and NPC 
+		int PC = 0;
+		int NPC = 0;
+		// initialize instruction memory
+		vector<string> instructionMem;
+		//create and intialize data memory
+		int dataMem[8] = { 0,0,0,0,0,0,0,0 };
+
+		// read instructions from file into instruction memory
+		//ifstream input("input1.txt");
+		ifstream input("input3.txt");
+		
+		string data;
+		if (input.good())
 		{
-			instructionMem.push_back(data);		//store line
+			while (getline(input, data)) //while there are lines to read, read them
+			{
+				instructionMem.push_back(data);		//store line
+			}
 		}
-	}
-	input.close();
-	printf("Simulation Begins \n");
+		input.close();
+		printf("Simulation Begins \n");
 
-	vector<string> decodedInstruction;
+		vector<string> decodedInstruction;
+		string currentInstruction;
+		int branch = 0;
 
-	//While there are instructions to process
-	while(PC < instructionMem.size()) {
-		//get the next instruciton
-		string currentInstruction = instructionMem[PC];
-		// Increment the PC and store the value in NPC
-		//NPC = PC++;
-		//decode the instruction and read registers
-		decodedInstruction = decodeInstruction(currentInstruction);
-		//ALU performs the operation
-		ALU(decodedInstruction);
-		//memory access/ branch completion
-		//write back
-		PC++;
+		//While there are instructions to process
+		while (PC < instructionMem.size()) {
+			//get the next instruciton
+			currentInstruction = instructionMem[PC];
+			// Increment the PC and store the value in NPC
+			//NPC = PC++;
+			//decode the instruction and read registers
+			decodedInstruction = decodeInstruction(currentInstruction);
+			//ALU performs the operation
+			branch = ALU(decodedInstruction);
+			//memory access/ branch completion
+			//write back
+			if (branch < 0) {
+				PC = PC + branch;
+				//PC++;
+			}
+			else if (branch > 0) {
+				PC = PC - branch;
+				PC--;
+			}
+			else if (branch == 0)
+				PC++;
+		}
 
-	}
-
-	cout << reg[11];
+		
+			printf("Simulation results: \n");
+			//cout << reg[11];
+			cout << reg[22];
+	
 }
 
